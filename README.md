f925947d32e375f35611ea9098051827beaa6a5f
nick <nick@foxsec.net>
2018-04-18 14:32:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# recon-dradis
+recon-dradis attempts to automate hostname reconaissance by running various tools to discover in-scope hostnames and automatically updating nodes in Dradis with this information. The following tools are currently supported (with more to come):
+1. Sublist3r (https://github.com/aboul3la/Sublist3r)
+## Warning
+Please note that this tool is still in very early stages of development and may contain bugs. Please try it against a test/mock Dradis project to make sure you don't lose any important data in the event something goes wrong.
+## Getting Started
+1. Clone the repository (be sure to include `--recurse-submodules`):  
+```
+$ git clone --recurse-submodules https://github.com/njfox/recon-dradis && cd recon-dradis
+```
+2. Add your Dradis API key and URL to `dradis_config.json` (**note**: do not include the `/pro` or anything else in the path)  
+#### IMPORTANT
+3. Tell git to ignore further changes to `dradis_config.json` so you don't accidentally push secrets to GitHub:  
+```
+$ git update-index --assume-unchanged dradis_config.json
+```
+4. Run the tool. At minimum, you must specify a domain with `-d` and a Dradis project name with `-p`:
+```
+$ ./recon-dradis.py -d example.com -p "Example Penetration Test"
+```
+5. Optionally, you may specify a line-separated list of target domains with `-f`/`--domain-file`
+```
+$ ./recon-dradis.py -f domains.txt -p "Example Penetration Test"
+```
+6. You may also specify a line-separated list of in-scope network ranges with `-s`/`--scope-file`. Only IP addresses in these ranges will be modified in the Dradis project. If no scope file is defined, all IP addresses discovered will be considered in-scope.
+```
+$ ./recon-dradis.py -f domains.txt -s scope.txt -p "Example Penetration Test"
+```
+## Contributing
+1. Create a new branch for your feature:  
+```
+$ git checkout -b my_new_feature
+```
+2. Make code changes and add commits
+3. Push commits to the branch:  
+```
+$ git push origin my_new_feature
+```
+4. When your feature is complete, create a new Pull Request so it can be merged into `master`.
---
+++ recon-dradis.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+
+import ipaddress
+import getopt
+import sys
+import json
+import sublist3r
+import dns.resolver
+from pydradis.pydradis import Pydradis
+
+USAGE = "Usage: %s [-d <domain>] [-f <domain file>] [-s <scope file>] -p '<project name>'"
+
+def parse_cli_options(argv):
+    domains = []
+    domain_filename = ""
+    scope_subnets = []
+    scope_filename = ""
+    project_name = ""
+
+    try:
+        opts, args = getopt.getopt(argv[1:],"hd:f:s:p:",["help","domain=","domain-file=","scope-file=","project-name="])
+    except getopt.GetoptError:
+        print "[+] Error: Invalid options specified."
+        print USAGE % argv[0]
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print USAGE % argv[0]
+            sys.exit()
+        elif opt in ("-d", "--domain"):
+            domains.append(arg)
+        elif opt in ("-f", "--domain-file"):
+            domain_filename = arg
+        elif opt in ("-s", "--scope-file"):
+            scope_filename = arg
+        elif opt in ("-p", "--project-name"):
+            project_name = arg
+
+    if domain_filename:
+        try:
+            f = open(domain_filename, "r")
+            for domain in f:
+                domains.append(domain.strip())
+            f.close()
+        except IOError:
+            print "[+] Error: Could not open domain file"
+            sys.exit(2)
+
+    if scope_filename:
+        try:
+            f = open(scope_filename, "r")
+            for line in f:
+                network = ipaddress.ip_network(line.strip().decode())
+                scope_subnets.append(network)
+            f.close()
+            if len(scope_subnets) == 0:
+                print "[+] Error: Could not parse IP ranges in scope file."
+                sys.exit(2)
+        except IOError:
+            print "[+] Error: Could not open scope file"
+            sys.exit(2)
+
+    if len(domains) == 0:
+        print "[+] Error: Could not parse target domains"
+        sys.exit(2)
+    if not project_name:
+        print "[+] Error: Invalid project name"
+        sys.exit(2)
+    
+    return (domains, scope_subnets, project_name)
+
+
+def init_dradis():
+    try:
+        config = open("dradis_config.json", "r")
+        info = json.load(config)
+    except IOError:
+        print "[+] Error: Error opening Dradis configuration file."
+        sys.exit(2)
+
+    config.close()
+    api_key = info["api_key"]
+    if not api_key:
+        print "[+] Error: dradis_config.json must include a valid API key."
+        sys.exit(2)
+
+    dradis_url = info["dradis_url"]
+    if not dradis_url:
+        print "[+] Error: dradis_config.json must include a valid Dradis URL."
+        sys.exit(2)
+    
+    debug = info["debug"]
+    verify = info["verify"]
+
+    return Pydradis(api_key, dradis_url, debug, verify)
+
+
+def find_project_id(pd, name):
+    project_id = pd.find_project(name)
+    if project_id == None:
+        print "[+] Error: Could not find project with name '%s'." % name
+        sys.exit(2)
+    
+    return project_id
+
+
+def find_subdomains(domain):
+    print "[+] Running sublist3r to find subdomains of %s..." % domain
+    return sublist3r.main(domain, 40, '', ports= None, silent=True, verbose=False, enable_bruteforce= False, engines=None)
+
+
+def is_in_scope(ip, scope):
+    for network in scope:
+        netw = int(network.network_address)
+        mask = int(network.netmask)
+        a = int(ipaddress.ip_address(ip.decode()))
+        if (a & mask) == netw:
+            return True
+    
+    return False
+
+def locate_node(pd, project_id, ip):
+    return pd.find_node(project_id, "plugin.output/" + ip)
+
+
+def update_hostnames(pd, project_id, node_id, hostnames):
+    nmap_host_note_id = pd.find_note(project_id, node_id, ["Hostnames"])[0][1]
+    nmap_host_note = pd.get_note(project_id, node_id, nmap_host_note_id)
+    ip = nmap_host_note["fields"]["IP"]
+    text = nmap_host_note["text"]
+    
+    print "[+] Updating hostnames for %s" % ip
+    hostnames_index = text.find("#[Hostnames]#")
+    hostnames_end_index = text.find("#[OS]#")
+    hostnames_field = text[hostnames_index:hostnames_end_index].strip()
+    hostnames_field += "\r\n"
+
+    for hostname in hostnames:
+        if hostname not in hostnames_field:
+            print "[+] Adding %s to %s" % (hostname, ip)
+            hostnames_field += hostname + "\r\n"
+        else:
+            print "[+] Hostname %s already exists for %s" % (hostname, ip)
+
+    hostnames_field += "\r\n"
+    
+    updated_text = text[:hostnames_index] + hostnames_field + text[hostnames_end_index:]
+    pd.update_note(project_id, node_id, nmap_host_note_id, updated_text)
+
+
+if __name__ == "__main__":
+    domains, scope, project_name = parse_cli_options(sys.argv)
+    if len(scope) == 0:
+        print "[+] No scope defined. Assuming all IP addresses are in-scope."
+        scope.append(ipaddress.ip_network("0.0.0.0/0".decode()))
+    
+    pd = init_dradis()
+    project_id = find_project_id(pd, project_name)
+
+    Resolver = dns.resolver.Resolver()
+    Resolver.nameservers = ['8.8.8.8', '8.8.4.4']
+    results = {}
+    for domain in domains:
+        results[domain] = find_subdomains(domain)
+        print "[+] Found %d in-scope subdomains for %s" % (len(results[domain]), domain)
+    
+    print "[+] Resolving subdomains in DNS..."
+    nodes = {}
+    for domain, subdomains in results.iteritems():
+        for subdomain in subdomains:
+            try:
+                ip = Resolver.query(subdomain, 'A')[0].to_text()
+            except:
+                continue
+            if ip:
+                if is_in_scope(ip, scope):
+                    try:
+                        print "[+] %s resolves to %s" % (subdomain, ip)
+                        nodes[ip].append(subdomain)
+                    except KeyError:
+                        nodes[ip] = [subdomain]
+    
+    for node in nodes.keys():
+        node_id = locate_node(pd, project_id, node)
+        if not node_id:
+            print "[-] %s is in-scope but there was no node found in Dradis. Double-check your Nmap scan." % node
+            continue
+        update_hostnames(pd, project_id, node_id, nodes[node])
